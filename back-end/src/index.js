553ca320f17db1d9797ecb9061ab3c270f3da180
Fernando Lisboa <fernando.costa@ccc.ufcg.edu.br>
2021-10-18 11:11:53 -0300

    add: crud customers

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -1,11 +1,14 @@
 import pg from "pg";
 import express from "express";
 import joi from "joi";
+import joidate from "@joi/date";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+const joi = joi.extend(joidate);
 const { Pool } = pg;
 
 const categorySchema = joi.object({
@@ -20,10 +23,16 @@ const gameSchema = joi.object({
 	pricePerDay: joi.number().integer().min(1).required(),
 });
 
-
-const customersSchema = joi.object({
-	
-})
+export const customerSchema = joi.object({
+	name: joi.string().min(1).required(),
+	phone: joi.string().min(10).max(11).required(),
+	cpf: joi
+		.string()
+		.length(11)
+		.pattern(/^[0-9]+$/)
+		.required(),
+	birthday: joi.date().format("YYYY MM DD").utc().required(),
+});
 
 const userSchema = joi.object({
 	username: joi.string().alphanum().min(3).max(30).required(),
@@ -67,9 +76,7 @@ app.post("/categories", async (req, res) => {
 		]);
 		if (duplicateCheck.rows.length !== 0) return res.sendStatus(409);
 
-		const newCategory = await connection.query("INSERT INTO categories (name) VALUES ($1);", [
-			name,
-		]);
+		await connection.query("INSERT INTO categories (name) VALUES ($1);", [name]);
 
 		return res.status(200).send(req.body);
 	} catch (error) {
@@ -114,24 +121,101 @@ app.post("/games", async (req, res) => {
 		]);
 		if (categoryCheck.rows.length === 0) return res.sendStatus(400);
 
-		console.log("gabrueru");
-		const newGame = await connection.query(
+		await connection.query(
 			`INSERT INTO games (name,image,"stockTotal","categoryId","pricePerDay") 
-													   VALUES ($1,$2,$3,$4,$5);`,
+			        VALUES ($1,$2,$3,$4,$5);`,
 			[name, image, stockTotal, categoryId, pricePerDay]
 		);
 
-		console.log("gabrueru");
 		return res.status(200).send(req.body);
 	} catch (err) {
 		return res.sendStatus(500);
 	}
 });
 
-app.get("/customers", (req,res)=>{
-	try{
-		const validation = 
+app.get("/customers", async (req, res) => {
+	try {
+		const { cpf } = req.query;
+		let customersList;
+
+		if (req.query) {
+			customersList = await connection.query("SELECT * FROM customers WHERE cpf iLIKE $1;", [
+				`${cpf}%`,
+			]);
+		} else {
+			customersList = await connection.query("SELECT * FROM customers;");
+		}
+
+		return res.status(200).send(customersList.rows);
+	} catch (err) {
+		console.log(err);
+		return res.sendStatus(500);
 	}
-})
+});
+
+app.get("/customers/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const customersList = await connection.query("SELECT * FROM customers WHERE id = $1;", [id]);
+		if (customersList.rows.length === 0) return res.sendStatus(404);
+
+		return res.status(200).send(customersList.rows);
+	} catch (err) {
+		console.log(err);
+		return res.sendStatus(500);
+	}
+});
+
+app.post("/customers", async (req, res) => {
+	try {
+		const validation = customerSchema.validate(req.body);
+		if (validation.error) return res.sendStatus(400);
+
+		const { name, phone, cpf, birthday } = req.body;
+
+		const duplicateCheck = await connection.query("SELECT * FROM customers WHERE cpf = $1;", [cpf]);
+		if (duplicateCheck.rows.length !== 0) return res.sendStatus(409);
+
+		await connection.query(
+			`INSERT INTO customers (name, phone, cpf, birthday)
+             	VALUES ($1, $2, $3, $4);`,
+			[name, phone, cpf, birthday]
+		);
+
+		return res.sendStatus(201);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+});
+
+app.put("/customers/:id", async (req, res) => {
+	try {
+		const { id } = req.params;
+		const { name, phone, cpf, birthday } = req.body;
+
+		const validation = customerSchema.validate(req.body);
+		if (validation.error) return res.sendStatus(400);
+
+		const idExistsCheck = connection.query("SELECT * FROM customers WHERE id = $1;", [id]);
+		if (idExistsCheck.rows.length === 0) return res.sendStatus(404);
+
+		const cpfCheck = connection.query("SELECT * FROM customers WHERE cpf = $1;", [cpf]);
+		if (cpfCheck.rows.length !== 0 && cpfCheck.rows[0].id !== id) return res.sendStatus(409);
+
+		await connection.query(
+			`UPDATE 
+                customers
+             	SET (name = $1,phone = $2,cpf = $3, birthday = $4)
+             	WHERE id = $5;`,
+			[name, phone, cpf, birthday, id]
+		);
+
+		return res.sendStatus(200);
+	} catch (err) {
+		console.log(err);
+		res.sendStatus(500);
+	}
+});
 
 app.listen(4000);
